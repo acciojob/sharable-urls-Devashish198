a017763d7cf1c46a0f30edaba51d0088a8e54c11
Devashish198 <dev.ashish.shukla281977@gmail.com>
2024-12-04 23:44:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-// your code here
+document.getElementById('myForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  var name = document.getElementById('name').value;
+  var year = document.getElementById('year').value;
+  
+  document.getElementById('url').textContent = `https://localhost:8080/?name=${name}&year=${year}`;
+});
